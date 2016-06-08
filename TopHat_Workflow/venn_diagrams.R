9288e6a3df75d5ad38623a9924ef1d545b7045d9
cwarden45 <cwarden45@gmail.com>
2016-06-07 17:43:45 -0700

    Update venn_diagrams.R

--- TopHat_Workflow/venn_diagrams.R
+++ TopHat_Workflow/venn_diagrams.R
@@ -8,9 +8,9 @@ set2.up = as.character(table2$symbol[table2$status == "Trt2 Up"])
 set2.down = as.character(table2$symbol[table2$status == "Trt2 Down"])
 
 gene.list = list(UP1=set1.up,
-				DOWN1=set1.down,
-				UP2=set2.up,
-				DOWN2=set2.down)
+		DOWN1=set1.down,
+		UP2=set2.up,
+		DOWN2=set2.down)
 vennObj = Venn(gene.list)
 
 png("venn.png")
