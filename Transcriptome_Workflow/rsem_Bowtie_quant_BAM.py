9f7d12492bd4c5d25a6ed03e53464c14770f316f
Charles Warden <cwarden45@gmail.com>
2016-07-01 14:34:25 -0700

    Update rsem_Bowtie_quant_BAM.py

--- Transcriptome_Workflow/rsem_Bowtie_quant_BAM.py
+++ Transcriptome_Workflow/rsem_Bowtie_quant_BAM.py
@@ -2,13 +2,6 @@ import sys
 import re
 import os
 
-
-mapFile = "/path/to/RSEM_Bowtie_index/transcript_to_gene.txt"
-refFa = "/path/to/RSEM_Bowtie_index/gencode.v24.transcriptID.fa"
-rsemIndex = "/path/to/RSEM_Bowtie_index/Bowtie_gencode_v24"
-command = "/opt/RSEM/rsem-prepare-reference --bowtie --bowtie-path /opt/bowtie --transcript-to-gene-map " + mapFile + " " + refFa + " " + rsemIndex
-os.system(command)
-
 threads = 4
 quantFolder = "RSEM_Bowtie_FQ"
 alignmentFolder = "Bowtie_alignment"
