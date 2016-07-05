26f574d9eeaacf163bdf1ee6e71e28be3f2461fe
Charles Warden <cwarden45@gmail.com>
2016-07-05 12:00:35 -0700

    Add files via upload

--- Transcriptome_Workflow/salmon_quant.py
+++ Transcriptome_Workflow/salmon_quant.py
@@ -1,12 +1,38 @@
 import sys
 import re
 import os
+import subprocess
 
-threads = 2
-ref = "/path/to/index_folder"
-quantFolder = "salmon_quant"
-readsFolder = "Reads"
+parameterFile = "parameters.txt"
 
+threads = ""
+ref = ""
+quantFolder = ""
+readsFolder = ""
+
+inHandle = open(parameterFile)
+lines = inHandle.readlines()
+			
+for line in lines:
+	line = re.sub("\n","",line)
+	line = re.sub("\r","",line)
+	
+	lineInfo = line.split("\t")
+	param = lineInfo[0]
+	value = lineInfo[1]
+	
+	if param == "Threads":
+		threads = value
+		
+	if param == "Salmon_index":
+		ref = value
+
+	if param == "Raw_Code_MAC":
+		quantFolder = value
+		
+	if param == "Reads_Folder_MAC":
+		readsFolder = value	
+		
 finishedSamples = ()
 fileResults = os.listdir(readsFolder)
 
@@ -23,7 +49,7 @@ for file in fileResults:
 			os.system(command)
 									
 			read1 = readsFolder + "/" + file
-					
-			command = "/opt/salmon/bin/salmon quant -l U -i " + ref +  " -p " + str(threads) + " -o " + outputSubfolder + " -r " + read1
+
+			command = "/opt/salmon/bin/salmon quant -l U -i " + ref +  " -p " + threads + " -o " + outputSubfolder + " -r " + read1
 			os.system(command)
 
