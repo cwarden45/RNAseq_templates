cdc72ed7a153ed3ae810e57e7bd75d4d2da5fe19
Charles Warden <cwarden45@gmail.com>
2016-09-28 10:48:45 -0700

    Add files via upload

---
+++ TopHat_Workflow/cluster_HTseq_counts.py
@@ -0,0 +1,83 @@
+import sys
+import re
+import os
+
+parameterFile = "parameters.txt"
+finishedSamples = ()
+
+alignmentFolder = ""
+gtf_file = ""
+lncRNAgtf = ""
+email = ""
+
+inHandle = open(parameterFile)
+lines = inHandle.readlines()
+			
+for line in lines:
+	line = re.sub("\n","",line)
+	line = re.sub("\r","",line)
+	
+	lineInfo = line.split("\t")
+	param = lineInfo[0]
+	value = lineInfo[1]
+	
+	if param == "Alignment_Folder_MAC":
+		alignmentFolder = value
+		
+	if param == "txGTF_MAC":
+		gtf_file = value
+
+	if param == "lncRNA_GTF_MAC":
+		lncRNAgtf = value		
+
+	if param == "Cluster_Email":
+		email = value
+		
+fileResults = os.listdir(alignmentFolder)
+
+jobCount = 0
+
+for file in fileResults:
+	result = re.search(".bam$",file)
+	fullPath = os.path.join(alignmentFolder, file)
+	
+	if result:
+		sample = re.sub(".bam$","",file)
+		sortResult = re.search(".name.sort.bam",file)
+		
+		if not sortResult:
+			jobCount += 1
+		
+		if (sample not in finishedSamples) and (not sortResult):
+			print sample
+	
+			shellScript = "htseq_" + sample + ".sh"
+			outHandle = open(shellScript, "w")
+			text = "#!/bin/bash\n"
+			text = text + "#$ -M "+email+"\n"
+			text = text + "#$ -m bea\n"
+			text = text + "#$ -N cwHT"+str(jobCount)+"\n"
+			text = text + "#$ -q all.q\n"
+			text = text + "#$ -j yes\n"
+			text = text + "#$ -o cwHT"+str(jobCount)+".log\n"
+			text = text + "#$ -cwd\n"
+			text = text + "#$ -V\n"
+			outHandle.write(text)
+	
+			nameSortedBam = sample + ".name.sort.bam"
+			sortPrefix = re.sub(".bam$","",nameSortedBam)
+			text = "samtools sort -n " + fullPath + " " + sortPrefix + "\n"
+			outHandle.write(text)
+		
+			countsFile = sample + "_gene_counts.txt"
+			text = "htseq-count -f bam -s no " + nameSortedBam + " " + gtf_file + " > " + countsFile + "\n"
+			outHandle.write(text)
+			
+			countsFile = sample + "_lncRNA_counts.txt"
+			text = "htseq-count -f bam -s no -i gene_name " + nameSortedBam + " " + lncRNAgtf + " > " + countsFile + "\n"
+			#os.system(command)
+	
+			text = "rm " + nameSortedBam + "\n"
+			outHandle.write(text)
+
+		
