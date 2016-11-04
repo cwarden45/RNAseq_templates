bbff6871ea4cdf65b51155711f9f00db152f1f24
Charles Warden <cwarden45@gmail.com>
2016-11-03 18:05:40 -0700

    Add files via upload

---
+++ Mutation_Workflow/run_VarScan.py
@@ -0,0 +1,16 @@
+import sys
+import re
+import os
+
+normal = "path/to/tumor.pileup"
+tumor = "path/to/normal.pileup"
+varscanPrefix = ""
+strand = ""
+java_mem = "10g"
+
+strandFilter = "1"
+if strand != "no":
+	strandFilter = "0"
+
+command = "java -Xmx" + java_mem+ " -jar /opt/varscan/VarScan.v2.4.2.jar somatic " + normal + " " + tumor + " " + varscanPrefix + " --min-var-freq 0.3 --min-avg-qual 20 --p-value 0.01 --somatic-p-value 0.01 --min-coverage-normal 10 --min-coverage-tumor 10 --strand-filter " + strandFilter
+os.system(command)
---
+++ Mutation_Workflow/run_VarScan_cluster.py
@@ -0,0 +1,33 @@
+import sys
+import re
+import os
+
+normal = "path/to/tumor.pileup"
+tumor = "path/to/normal.pileup"
+varscanPrefix = ""
+
+varscan_jar = "/path/to/VarScan.v2.4.2.jar"
+java_mem = "16g"
+email = ""
+strand = ""
+
+strandFilter = "1"
+if strand != "no":
+	strandFilter = "0"
+
+shellScript = varscanPrefix + ".sh"
+outHandle = open(shellScript, "w")
+text = "#!/bin/bash\n"
+text = text + "#$ -M "+email+"\n"
+text = text + "#$ -m bea\n"
+text = text + "#$ -N VSsomatic\n"
+text = text + "#$ -q all.q\n"
+text = text + "#$ -l vf=16G\n"
+text = text + "#$ -j yes\n"
+text = text + "#$ -o VSsomatic.log\n"
+text = text + "#$ -cwd\n"
+text = text + "#$ -V\n"
+outHandle.write(text)
+	
+text = "java -Xmx" + java_mem+ " -jar "+varscan_jar+" somatic " + normal + " " + tumor + " " + varscanPrefix + " --min-var-freq 0.3 --min-avg-qual 20 --p-value 0.01 --somatic-p-value 0.01 --min-coverage-normal 10 --min-coverage-tumor 10 --strand-filter " + strandFilter
+outHandle.write(text)
