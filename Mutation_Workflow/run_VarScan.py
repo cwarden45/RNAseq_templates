50ccd75b25c32c04454b0bf6764ee1793ad744b0
Charles Warden <cwarden45@gmail.com>
2016-11-04 11:10:02 -0700

    Update run_VarScan.py

--- Mutation_Workflow/run_VarScan.py
+++ Mutation_Workflow/run_VarScan.py
@@ -12,5 +12,6 @@ strandFilter = "1"
 if strand != "no":
 	strandFilter = "0"
 
+#you can create .vcf with --output-vcf, but then you have to parse out somatic variants and combine SNP and indel files
 command = "java -Xmx" + java_mem+ " -jar /opt/varscan/VarScan.v2.4.2.jar somatic " + normal + " " + tumor + " " + varscanPrefix + " --min-var-freq 0.3 --min-avg-qual 20 --p-value 0.01 --somatic-p-value 0.01 --min-coverage-normal 10 --min-coverage-tumor 10 --strand-filter " + strandFilter
 os.system(command)
