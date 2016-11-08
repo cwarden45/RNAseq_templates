56937f1f29ce8bf55153ac37fd4a3432be5e4883
Charles Warden <cwarden45@gmail.com>
2016-11-07 18:36:20 -0800

    Update batch_corrected_heatmap.R

--- TopHat_Workflow/batch_corrected_heatmap.R
+++ TopHat_Workflow/batch_corrected_heatmap.R
@@ -102,5 +102,6 @@ print(dim(temp.rpkm))
 		png(file = heatmap.file)
 		heatmap.2(std.expr, col=colorpanel(33, low="blue", mid="black", high="red"), density.info="none", key=TRUE,
 					 RowSideColors=labelColors, trace="none", margins = c(5,15))
+		legend("topright", legend=group.levels,	col=color.palette, pch=15, cex=0.7)
 		dev.off()
 	}#end else
