40007fe6ef4dc447d0d28397a9c62e6e7a3fa2aa
Charles Warden <cwarden45@gmail.com>
2016-12-23 11:23:27 -0800

    Add files via upload

---
+++ TopHat_Workflow/reformat_RPKM_for_bdfunc.R
@@ -0,0 +1,20 @@
+in.file = "log2_rpkm.txt"
+out.file = "log2_rpkm_GROUP.txt"
+meta.file = "sample_description.txt"
+groupID = "Group"
+
+meta.table = read.table(meta.file,head=T,sep="\t")
+sampleID = meta.table$userID
+group = meta.table[,groupID]
+
+expr.table = read.table(in.file,head=T,sep="\t")
+rpkm.mat = expr.table[,match(sampleID, names(expr.table))]
+colnames(rpkm.mat)=group
+
+output.table = data.frame(gene=expr.table$symbol, rpkm.mat)
+colnames(output.table) = c("gene",as.character(group))
+write.table(output.table, out.file, sep="\t", row.names=F, quote=F)
+
+#manually change header...
+#BD-Func
+#groupA	groupB groupA groupB groupA groupB
