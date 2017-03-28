f60e7186e4f846ca0b6ca3e5b22649026be26da1
Charles Warden <cwarden45@gmail.com>
2017-03-28 13:58:33 -0700

    Add files via upload

---
+++ Splicing_Workflow/revise_results.R
@@ -0,0 +1,31 @@
+new.FDR.cutoff = 0.25
+
+param.table = read.table("parameters.txt", header=T, sep="\t")
+comp.name=as.character(param.table$Value[param.table$Parameter == "comp_name"])
+count.folder=as.character(param.table$Value[param.table$Parameter == "QoRTs_Merged_Folder"])
+lib.type=as.character(param.table$Value[param.table$Parameter == "pairing"])
+user.folder = as.character(param.table$Value[param.table$Parameter == "Result_Folder"])
+
+plot.folder = paste(user.folder,"/DSG/",comp.name,"/",sep="")
+
+if (lib.type == "SE"){
+	lib.type = "single-end"
+}else if (lib.type == "PE"){
+	lib.type = "paired-end"
+}else{
+	stop(paste("Need to map `pairing` to `single-end` or `paired-end`:",lib.type))
+}
+
+library("JunctionSeq")
+
+jscsImage = paste(count.folder,"/",comp.name,"/jscs.RData",sep="")
+load(jscsImage)
+
+raw.output.folder = paste(count.folder,"/",comp.name,"/",sep="")
+plot.folder = paste(user.folder,"/DSG/",comp.name,"/",sep="")
+
+writeCompleteResults(jscs, outfile.prefix=raw.output.folder, save.jscs = FALSE,
+						FDR.threshold = new.FDR.cutoff, gzip.output = FALSE)
+					
+buildAllPlots(jscs=jscs, outfile.prefix = plot.folder, sequencing.type = lib.type,
+				use.plotting.device = "png",FDR.threshold = new.FDR.cutoff)
