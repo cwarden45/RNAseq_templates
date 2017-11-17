d845a546ddfb4af74c2eeb376e3d9df51138f157
Charles Warden <cwarden45@gmail.com>
2017-11-17 10:19:12 -0800

    Update run_GSEA.py

--- TopHat_Workflow/run_GSEA.py
+++ TopHat_Workflow/run_GSEA.py
@@ -10,8 +10,8 @@ outputFolder = "../Result/GSEA/"
 comparisons = ["Trt_versus_Cont"]
 
 signature_folder = "/path/to/MSigDB"
-signature_files = ["c5.bp.v6.0.symbols.gmt","c5.mf.v6.0.symbols.gmt","c6.all.v6.0.symbols.gmt","h.all.v6.0.symbols.gmt"]
-#signature_files = ["c2.all.v6.0.symbols.gmt"]
+signature_files = ["c5.bp.v6.1.symbols.gmt","c5.mf.v6.1.symbols.gmt","c6.all.v6.1.symbols.gmt","h.all.v6.1.symbols.gmt"]
+#signature_files = ["c2.all.v6.1.symbols.gmt"]
 
 mac_GSEA_jar = "/path/to/gsea2-2.2.2.jar"
 javaMem = "8g"
